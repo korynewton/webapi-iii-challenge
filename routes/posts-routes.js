71a97475ff66d1feb53f065a605872271d1de673
korynewton <korynewtondev@gmail.com>
2019-03-20 13:32:41 -0700

    DELETE user functionality working

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router()
 
+router.use(express.json());
+
 const Db = require('../data/helpers/postDb') 
+const UsersDb = require('../data/helpers/userDb')
 
-
+//gets all posts in db
 router.get('/', async (req, res) => {
     try {
         const users = await Db.get()
@@ -14,6 +17,8 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+//gets post by post id
 router.get('/:id', async (req,res) => {
     const { id } = req.params
     try {
@@ -27,5 +32,25 @@ router.get('/:id', async (req,res) => {
         res.status(500).json({ error: "There was an error in retrieving that post" })
     }
 })
+
+//add post to db by
+// router.post('/:id', async (req, res) => {
+//     const { id } = req.params
+//     const post = req.body
+//     try {
+//         const usersPosts = await UsersDb.getUserPosts(id)
+//         const id = usersPosts[-1].id + 1
+//         const postedBy = usersPosts[-1].postedBy
+//         post.id = id;
+//         post.postedBy = postedBy;
+//         usersPosts.push(post)
+//         res.status(200).json(usersPosts)
+//     } catch {
+//         res.status(500).json({ error: "Failed retrieving users posts" })
+//     }
+// })
+
+
+
 
 module.exports = router;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -30,12 +30,25 @@ router.get('/:id', async (req,res) => {
 })
 
 router.post('/', async (req, res) => {
-    console.log(req.body)
     try {
         const userToAdd = await Db.insert(req.body)
         res.status(200).json(userToAdd)
     } catch {
         res.status(500).json({ error: "Failed adding user" })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const remove = await Db.remove(id)
+        if (!remove) {
+            res.status(404).json({ error : "User not found" })
+        } else {
+            res.status(200).json({ message: "user deleted" })
+        }
+    } catch {
+        res.status(500).json({ error: "error in deleting User" })
     }
 })
 
