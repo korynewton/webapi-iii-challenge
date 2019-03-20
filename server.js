4b43f3635c9bbae5cf4a1a82a97b1ca9e6c8e8cc
korynewton <korynewtondev@gmail.com>
2019-03-20 12:20:04 -0700

    Users and posts routers added

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const usersRoutes = require('./routes/users-routes')
+const postsRoutes = require('./routes/posts-routes')
+
+
 const server = express();
 
 //handle routing
@@ -7,6 +11,9 @@ const server = express();
 server.get('/', (req, res) => {
     res.status(200).json({  message:' GET success'})
 })
+
+server.use('/users', usersRoutes)
+server.use('/posts', postsRoutes)
 
 
 
