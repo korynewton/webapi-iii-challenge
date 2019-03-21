b13ce460e6229ded61c6e66c7f350ccef46ff1f7
korynewton <korynewtondev@gmail.com>
2019-03-21 10:58:45 -0700

    changed console.log message

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 4000;
 const greeting = process.env.GREETING;
 
 server.listen(4000 , () => {
-    console.log('\n** ${greeting} up and running on port ${port}**')
+    console.log(`\n** ${greeting}, we are up and running on port ${port}**`)
 })
