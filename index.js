a6ee3fe4927caf1a8ff63f6a421554b7d71c5a7c
korynewton <korynewtondev@gmail.com>
2019-03-21 11:58:36 -0700

    changed hard coded port number to be port variable in index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = require('./server');
 const port = process.env.PORT || 4000;
 const greeting = process.env.GREETING;
 
-server.listen(4000 , () => {
+server.listen(port , () => {
     console.log(`\n** ${greeting}, we are up and running on port ${port}**`)
 })
